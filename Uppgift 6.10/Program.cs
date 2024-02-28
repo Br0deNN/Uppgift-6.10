b0edf781d64d5a20710dd3209b1ae4107b3bdf63
7R1XY <theo.broden@gmail.com>
2024-02-28 10:19:03 +0100

    Add project files.

---
+++ Uppgift 6.10/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine($"Det finns {AntalTalIText(input)} stycken tal i meningen");
+    }
+
+    static int AntalTalIText(string input)
+    {
+        int gånger = 0;
+        string[] array = input.Split(' ');
+        for (int i = 0; i < array.Length; i++)
+        {
+            try
+            {
+                double x = double.Parse(array[i]);
+                gånger++;
+            }
+            catch 
+            {
+                
+            }
+        }
+        return gånger;
+    }
+}
